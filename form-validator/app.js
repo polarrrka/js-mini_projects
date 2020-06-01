f362323f9547891dfd6dd9cc19aa3faa085eb347
Jana Trnková <58814759+polarrrka@users.noreply.github.com>
2020-06-01 19:33:56 +0200

    udemy

---
+++ form-validator/app.js
@@ -0,0 +1,92 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+function showError(input, message) {
+  const formCtrl = input.parentElement;
+  formCtrl.className = 'form-control error';
+  const small = formCtrl.querySelector('small');
+  small.innerText = message;
+}
+
+function showSuccess(input) {
+  const formCtrl = input.parentElement;
+  formCtrl.className = 'form-control success';
+}
+
+function checkEmail (input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  // return re.test(String(email).toLowerCase());
+  if(re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'E-mail is not valid!');
+  }
+}
+
+function checkRequired(inputArr) {
+  inputArr.forEach(function(input) {
+    if(input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required!`)
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+function checkLength(input, min, max) {
+  if(input.value.length < min) {
+    showError(input, `${getFieldName(input)} must be at least ${min} characters!`);
+  } else if (input.value.length > max) {
+    showError(input, `${getFieldName(input)} must be less than ${max} characters!`);
+  } else {
+    showSuccess(input);
+  }
+}
+
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match!')
+  }
+}
+
+form.addEventListener('submit', function(e) {
+  checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
+
+/*   if (username.value === "") {
+    showError(username, "Username is required!");
+  } else {
+    showSuccess(username)
+  }
+
+  if (email.value === "") {
+    showError(email, "E-mail is required!");
+  } else if (!isValidEmail(email.value)) {
+    showError(email, "E-mail is not valid!")
+  } else {
+    showSuccess(email)
+  }
+
+  if (password.value === "") {
+    showError(password, "Password is required!");
+  } else {
+    showSuccess(password)
+  }
+
+  if (password2.value === "") {
+    showError(password2, "Password is required!");
+  } else {
+    showSuccess(password2)
+  } */
+  e.preventDefault();
+});

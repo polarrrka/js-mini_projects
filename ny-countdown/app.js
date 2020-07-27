b58d29156a24dfffac8db4b2b6557bc882c039bb
Jana Trnková <58814759+polarrrka@users.noreply.github.com>
2020-07-27 19:56:29 +0200

    udemy

---
+++ ny-countdown/app.js
@@ -0,0 +1,34 @@
+const days = document.getElementById('days'),
+      hours = document.getElementById('hours'),
+      minutes = document.getElementById('minutes'),
+      seconds = document.getElementById('seconds'),
+      countdown = document.getElementById('countdown'),
+      year = document.getElementById('year'),
+      loading = document.getElementById('loading');
+
+const currentYear = new Date().getFullYear(),
+      newYearTime = new Date(`January 01 ${currentYear + 1} 00:00`);
+
+year.innerText = currentYear + 1;
+
+function updateCountDown() {
+  const currentTime = new Date();
+  const diff = newYearTime - currentTime;
+  
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24),
+        h = Math.floor(diff / 1000 / 60 / 60) % 24,
+        m = Math.floor(diff / 1000 / 60) % 60,
+        s = Math.floor(diff / 1000) % 60;
+  
+  days.innerHTML = d;
+  hours.innerHTML = h < 10 ? '0' + h : h;
+  minutes.innerHTML = m < 10 ? '0' + m : m;
+  seconds.innerHTML = s < 10 ? '0' + s : s;
+}
+
+setTimeout(() => {
+  loading.remove();
+  countdown.style.display = 'flex';
+}, 1000);
+
+setInterval(updateCountDown, 1000);
